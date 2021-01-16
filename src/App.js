60c9376fb3c9d2c13523ccbd80fe64ac2b189870
mbobryshev <mbobryshev@luxoft.com>
2021-01-16 15:20:27 +0300

    show cars list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [cars, setCars] = useState(null)
+  const [accaunts, setAccauns] = useState(null)
+  useEffect(() => {
+    fetch('http://localhost:18000/cars').then(e => e.json()).then(setCars)
+
+    fetch('http://localhost:18000/accaunts').then(e => e.json()).then(console.log)
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +17,19 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <h3>Евть машины:</h3>
+          <ul>
+            {cars && cars.map( ({ Name, Cylinders }, ind) => {
+              return (<li key={ind}>
+                <div>{Name}</div>
+                <div>
+                  <span>Количество цилиндров:</span><span> {Cylinders}</span>
+                </div>
+              </li>)
+            })}
+          </ul>
+        </p>
       </header>
     </div>
   );
