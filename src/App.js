2e5951179c86d2429e64a017dfe2e3af6878620e
Artemy <artemij87work@gmail.com>
2021-02-10 18:02:42 +0300

    add first name from accaunts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [cars, setCars] = useState(null)
-  const [accaunts, setAccauns] = useState(null)
+  const [cars, setCars] = useState(null);
+  const [accaunts, setAccauns] = useState(null);
   useEffect(() => {
-    fetch('http://localhost:18000/cars').then(e => e.json()).then(setCars)
+    fetch("http://localhost:18000/cars")
+      .then((e) => e.json())
+      .then(setCars);
 
-    fetch('http://localhost:18000/accounts').then(e => e.json()).then(console.log)
+    fetch("http://localhost:18000/accounts")
+      .then((e) => e.json())
+      .then(setAccauns);
   }, []);
   return (
     <div className="App">
@@ -20,14 +24,32 @@ function App() {
         <p>
           <h3>Еcть машины:</h3>
           <ul>
-            {cars && cars.map( ({ Name, Cylinders }, ind) => {
-              return (<li key={ind}>
-                <div>{Name}</div>
-                <div>
-                  <span>Количество цилиндров:</span><span> {Cylinders}</span>
-                </div>
-              </li>)
-            })}
+            {cars && cars.map(({ Name, Cylinders }, ind) => {
+                return (
+                  <li key={ind}>
+                    <div>Марка: {Name}</div>
+                    <div>
+                      <span>Количество цилиндров:</span>
+                      <span> {Cylinders}</span>
+                    </div>
+                  </li>
+                );
+              })}
+          </ul>
+        </p>
+        <p>
+          <h3>Клиенты</h3>
+          <ul>
+            {accaunts && accaunts.map(({name}, ind) => {
+                return (
+                  <li key={ind}>
+                    <div>Имя: {name["first"]}</div>
+					<div>
+
+					</div>
+                  </li>
+                );
+              })}
           </ul>
         </p>
       </header>
